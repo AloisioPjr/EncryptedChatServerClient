ae65983b984d49ac4dc58beec7b73621dfda76ca
AloisioPjr <Aloisiopjr@outlook.com>
2023-11-14 22:20:05 +0000

    Add files via upload
    Comments added to the program.
    GUI started.
    program does not work with GUI.
    need to either delete the Jframe to run the program or implement the GUI fully.

--- EncryptedChat/src/encryptedchat/EncryptedChatClient.java
+++ EncryptedChat/src/encryptedchat/EncryptedChatClient.java
@@ -5,6 +5,7 @@
 package encryptedchat;
 
 import java.io.*;
+import static java.lang.System.exit;
 import java.net.*;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -12,11 +13,8 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -28,71 +26,76 @@ public class EncryptedChatClient {
 
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 12345);
-            System.out.println("Connected to server.");
+            Socket socket = new Socket("localhost", 12345);// connects to the server trough the port 12345
+            System.out.println("Connected to server.");// print a message to indicate the connection was successful   
+            new Thread(new ServerHandler(socket)).start(); //the socket is passed as an argument when creating a new instance of the class ServerHandler which is passed as an argument for a new thread 
 
-            
-            new Thread(new ClientReceiver(socket)).start();
-
-            
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e) {// print error message if theres an error with the socket
+            System.out.println("Error: Could not start Server-Client Connection");
         }
     }
 }
 
-class ClientReceiver implements Runnable {
+class ServerHandler implements Runnable {// this class will implement the Runnable interface which means it will have to override the run method
 
-    private static PrivateKey privateKey;
-    private static PublicKey publicKey;
-
+    //These are all the global variables used in the program
+    private PrivateKey privateKey;// 
+    private PublicKey publicKey;// 
+    private SecretKey aesKey;
     private Socket socket;
     private BufferedReader reader;
-    private ObjectOutputStream objectOutputStream;
-    
-    
-    private SecretKey aesKey;
     private BufferedWriter writer;
 
-    public ClientReceiver(Socket socket) {
-        this.socket = socket;
+    public ServerHandler(Socket socket) {// the constructor takes the socket as parameter
+        this.socket = socket;// initiate the socket with the value of the parameter
         try {
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));// initialize the buffered reader and writer
             writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            
 
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//to send
-            
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e) {// catch any error input output error that occur while initializing the buffers 
+            System.out.println("Error: Could not start message buffers");
         }
     }
 
     @Override
-    public void run() {
-        RSAKeyGenerator();
-        SendPublicKey(publicKey);
-        DecryptedAESKey();
-        listenForMessage();
+    public void run() {// the overriden run method calls the following methods
+        RSAKeyGenerator(); // firstly a public and a private key are generated 
+        SendPublicKey(publicKey);// the public Key  created previously is sent to the server
+        DecryptedAESKey();// this method decrypts the Symmetric key received from the Server by the method receivedEncryptedAESKey()
+        listenForMessage();// this method starts a new anonymous Thread and listen to messages while the user is still capable of sending messages they happens concurrently
+        sendMessage();// this method anables the client to send messages to the server
 
+    }
+
+    public void sendMessage() {
+        /* this method collects the input from the user usig a Scanner 
+         * object and sening it through the socket with the buffered writer
+         * this all happen inside of a loop while the the socket is connected
+         * and inside of a try catch in case of any input/output exception*/
         Scanner scanner = new Scanner(System.in);
-        System.out.println("This is an end-to-end ecypted chat. \nPlease enter your name:");
-        String clientName= scanner.nextLine();
+        System.out.println("This is an end-to-end encypted chat. \nPlease enter your name:");
+        String clientName = scanner.nextLine();
         System.out.println("Type your message...");
         String message;
         while (socket.isConnected()) {
             try {
                 message = scanner.nextLine();
-                writer.write(EncryptMessage(clientName+": "+message));
+                writer.write(EncryptMessage(clientName + ": " + message));
                 writer.newLine();
                 writer.flush();
             } catch (IOException ex) {
-                ex.printStackTrace();
+                System.out.println("Error: Could not send message to Server");
             }
+        }
+        if (socket.isClosed() || !socket.isConnected()) {
+            closeEverything(socket, reader, writer);
         }
     }
 
     public void listenForMessage() {
+        /* This method creates a concurrent new Thread that works only listening for messages from the Server and prints it on the screen.
+         * like the previous method all of this happen while the socket is connected with a try catch in case of any I/O exception 
+         */
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -102,55 +105,74 @@ class ClientReceiver implements Runnable {
                         msgFromGroupChat = reader.readLine();
                         System.out.println(DecryptMessage(msgFromGroupChat));
                     } catch (IOException e) {
+                        System.out.println("Error: Could not receive message from Server");
                         closeEverything(socket, reader, writer);
 
                     }
+                }
+                if (socket.isClosed() || !socket.isConnected()) {
+                    closeEverything(socket, reader, writer);
                 }
             }
         }).start();
 
     }
 
-    public static void RSAKeyGenerator() {
+    public void RSAKeyGenerator() {
+        /* This method generates the public and the private key necessary to encrypt and decrypt the Symetric Key
+         * the public and the private key are stored globaly so other methods can access it
+         * it happens inside of a try catch in case of exception 
+         */
         KeyPair keyPair = null;
         try {
             KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
             keyPairGen.initialize(2048);
             keyPair = keyPairGen.generateKeyPair();
             publicKey = keyPair.getPublic();
-            //System.out.println("publicKey----->" + publicKey);
+            //System.out.println("publicKey----->" + publicKey); // used for debugging
             privateKey = keyPair.getPrivate();
-            //System.out.println("privateKey----->" + privateKey);
+            //System.out.println("privateKey----->" + privateKey);// used for debugging
         } catch (NoSuchAlgorithmException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not generate RSA Key Pair");
         }
     }
 
     public void SendPublicKey(PublicKey publicKey) {
+        /* This method take a public key  object as parameter and send the public key object through a 
+         * Object Output Stream becaseu the buffered writer does not accept object type 
+         *  all inside of a try catch in case of any I/O exception 
+         */
         try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//to send
             objectOutputStream.writeObject(publicKey);
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not send Public Key to Server");
         }
     }
 
     public byte[] receivedEncryptedAESKey() {
-        //String  encryptedAESKeyString;
+        /* This method will receive from the buffered reader the String format of the encrypted Symmetric key 
+         * and convert it to byte array with Base64 Class and return it. In case of input/output exception there's a try catch 
+         */
         try {
-           
+
             String encryptedAESKeyString = reader.readLine();
             byte[] encryptedAESKeyBytes = Base64.getDecoder().decode(encryptedAESKeyString);
-            //System.out.println("encryptedAESKeyBytes----->" + Arrays.toString(encryptedAESKeyBytes));
-            //System.out.println("encryptedAESKeyBytes----->" + (encryptedAESKeyString));
+            // 
+            //System.out.println("encryptedAESKeyBytes----->" + Arrays.toString(encryptedAESKeyBytes));// used for debugging
+            //System.out.println("encryptedAESKeyBytes----->" + (encryptedAESKeyString));// used for debugging
             return encryptedAESKeyBytes;
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not receive encrypted Symmetric Key from Server");
         }
         return null;
     }
 
     public void DecryptedAESKey() {
-
+        /* This method takes the value byte array that the receivedEncryptedAESKey() method returns and
+         * initiates the Cipher on decrypt the Symmetric key with the public key 
+         * and assign it to the global variable aesKey
+         * all inside of a try catch error */
         try {
 
             byte[] receivedEncryptedAESKey = receivedEncryptedAESKey();
@@ -161,15 +183,21 @@ class ClientReceiver implements Runnable {
             byte[] decryptedAESKeyBytes = cipher.doFinal(receivedEncryptedAESKey);
 
             aesKey = new SecretKeySpec(decryptedAESKeyBytes, "AES");
-            //System.out.println("encryptedAESKeyBytes----->" + (aesKey));
+            //System.out.println("encryptedAESKeyBytes----->" + (aesKey));// used for debugging
 
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not decrypt Symmetric Key");
         }
 
     }
 
     public String EncryptMessage(String message) {
+        /* This method uses the cipher class together with the Symmetric key gathered earlier
+         * to encrypt the message passed as a parameter it it also uses the base64 class to 
+         * convert the byte array to string and return it. It is all inside of a try catch and 
+         * for security reasons none of the errors print stacktrace of the error itself, instead
+         * they print a message*/
+
         String encryptedMessage = null;
         try {
             Cipher cipher = Cipher.getInstance("AES");
@@ -178,39 +206,52 @@ class ClientReceiver implements Runnable {
             encryptedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
 
         } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not encrypt message with Symmetric Key ");
         }
         return encryptedMessage;
     }
 
     public String DecryptMessage(String message) {
+        /* This method uses the cipher class together with the Symmetric key stored globaly
+         * to decrypt the message passed as a parameter it uses the base64 class to 
+         * convert the String message to byte array and then after its passed through the cipher 
+         * and then byte array variable is converted to string and its returned.
+         * It is all inside of a try catch and for security reasons none of the errors 
+         * print stacktrace of the error itself, instead they print a message*/
         String decryptedMessage = null;
         try {
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.DECRYPT_MODE, aesKey);
-            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(message));
+            
+            byte[] decodedBytes = Base64.getDecoder().decode(message);
+        
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
             decryptedMessage = new String(decryptedBytes);
-
-        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
-            ex.printStackTrace();
+            
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException
+                | NoSuchAlgorithmException | NoSuchPaddingException ex) {
+            System.out.println("Error: Could not decrypt message with Symmetric Key ");
         }
         return decryptedMessage;
     }
 
-    private void closeEverything(Socket socket, BufferedReader reader, BufferedWriter bwriter) {
+    private void closeEverything(Socket socket, BufferedReader reader, BufferedWriter writer) {
+        /* This method checkes if the resources are not null then
+         * closes all the resources and it shuts downs the program*/
         try {
-            // Close resources (socket, reader, writer)
+
             if (reader != null) {
                 reader.close();
             }
-            if (bwriter != null) {
-                bwriter.close();
+            if (writer != null) {
+                writer.close();
             }
             if (socket != null) {
                 socket.close();
             }
+            exit(0);
         } catch (IOException e) {
-            e.printStackTrace(); // Print the stack trace if an IO exception occurs while closing resources
+            System.out.println("Error: There was an error when closing the Server-Client connection ");
         }
     }
 }
--- EncryptedChat/src/encryptedchat/EncryptedChatServer.java
+++ EncryptedChat/src/encryptedchat/EncryptedChatServer.java
@@ -6,21 +6,15 @@ package encryptedchat;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import static java.lang.System.exit;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.Scanner;
 import javax.crypto.BadPaddingException;
@@ -62,7 +56,7 @@ class ClientHandler implements Runnable {
     private Socket socket;
     private BufferedReader reader;
     
-    private ObjectInputStream objectInputStream;
+    
     private SecretKey aesKey;
     private BufferedWriter writer;
 
@@ -71,11 +65,11 @@ class ClientHandler implements Runnable {
         try {
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            objectInputStream = new ObjectInputStream(socket.getInputStream());//to receive the public key
+            
 
-            //System.out.println(Base64.getEncoder().encodeToString(AESKeyEncryptor()));
+            //System.out.println(Base64.getEncoder().encodeToString(AESKeyEncryptor())); // used for debugging
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error: Could not start Server-Client Connection");
         }
     }
 
@@ -83,6 +77,13 @@ class ClientHandler implements Runnable {
     public void run() {
         SendEncryptedAESKey();
         listenForMessage();
+        sendMessage();
+    }
+    public void sendMessage() {
+        /* this method collects the input from the Server usig a Scanner 
+         * object and sending it through the socket with the buffered writer
+         * this all happen inside of a loop while the the socket is connected
+         * and inside of a try catch in case of any input/output exception*/
         System.out.println("A new client has connected to the server.");
         Scanner scanner = new Scanner(System.in);
         String message;
@@ -93,12 +94,17 @@ class ClientHandler implements Runnable {
                 writer.newLine();
                 writer.flush();
             } catch (IOException ex) {
-                ex.printStackTrace();
+                System.out.println("Error: Could not send or receive message to Client");
             }
-        }
+        }if (socket.isClosed()||!socket.isConnected()){
+                    closeEverything(socket, reader, writer);
+                }
     }
 
     public void listenForMessage() {
+        /* This method creates a concurrent new Thread that works only listening for messages from the Client and prints it on the screen.
+         * like the previous method all of this happen while the socket is connected with a try catch in case of any I/O exception 
+         */
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -111,6 +117,8 @@ class ClientHandler implements Runnable {
                         closeEverything(socket, reader, writer);
 
                     }
+                }if (socket.isClosed()||!socket.isConnected()){
+                    closeEverything(socket, reader, writer);
                 }
             }
         }).start();
@@ -119,7 +127,8 @@ class ClientHandler implements Runnable {
 
     private void closeEverything(Socket socket, BufferedReader reader, BufferedWriter bwriter) {
         try {
-            //check if the resources (socket, reader, writer) are still valid before closing them
+            /* This method checkes if the resources are not null then
+             * closes all the resources and it shuts downs the program*/
             if (reader != null) {//
                 reader.close();
             }
@@ -130,54 +139,68 @@ class ClientHandler implements Runnable {
                 socket.close();
             }
         } catch (IOException e) {
-            e.printStackTrace(); // Print the stack trace if an IO exception occurs while closing resources
+            System.out.println("Error: There was an error when closing the Server-Client connection ");
         }
     }
 
     public static SecretKey AESKeyGenerator() {
+        /* This method generates the Symmetric Key size of 256 bytes and
+         * returns the SecretKey Object. It has a try catch in case of any error 
+         * during the generation of the key*/
         SecretKey aesKey = null;
         try {  
             KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
             keyGenerator.init(256);
             aesKey = keyGenerator.generateKey();
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            System.out.println("Error: Could not generate Symmetric Key ");
         }
-        //System.out.println("aesKey----->" + aesKey);
+        //System.out.println("aesKey----->" + aesKey);// used for debugging
         return aesKey;
     }
 
     public PublicKey ReceivedPublicKey() {
+        /* THis method receive the Prublic key from the Client through the socket in Object Input Stream 
+         * Then its assigned to a Public Key type of variable and returned it
+         * Try chatch are implemented in case of errors */
         PublicKey receivedPublicKey = null;
         try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());//to receive the public key
             receivedPublicKey = (PublicKey) objectInputStream.readObject();
         } catch (IOException | ClassNotFoundException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not receive Public Key from Client");
         }
-        //System.out.println("receivedPublicKey----->" + receivedPublicKey);
+        //System.out.println("receivedPublicKey----->" + receivedPublicKey);// used for debugging
         return receivedPublicKey;
     }
 
     public String AESKeyEncryptor() {
-
+        /* This method assigns the value of the Symmetric key from the method AESKeyGenerator() to the global variable aesKey and
+         * asssigns the value of the ReceivedPublicKey() to a local variable and
+         * encrypts using the cipher class to encrypt the Symmetric key with with the public Key 
+         * and returns the string value of the encrypted symetric key
+         * Try chatch are implemented in case of errors */
         try {
             aesKey = AESKeyGenerator();
             PublicKey receivedPublicKey = ReceivedPublicKey();
 
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
             cipher.init(Cipher.ENCRYPT_MODE, receivedPublicKey);
-            byte[] encryptedAESKeyBytes = cipher.doFinal(aesKey.getEncoded());//TODO: try changing to .getBytes()
+            byte[] encryptedAESKeyBytes = cipher.doFinal(aesKey.getEncoded());//
             String encryptedAESKeyString = Base64.getEncoder().encodeToString(encryptedAESKeyBytes);
-            //System.out.println("encryptedAESKeyBytes----->" + Arrays.toString(encryptedAESKeyBytes));
-            //System.out.println("encryptedAESKeyString----->" + (encryptedAESKeyString));
+            //System.out.println("encryptedAESKeyBytes----->" + Arrays.toString(encryptedAESKeyBytes));// used for debugging
+            //System.out.println("encryptedAESKeyString----->" + (encryptedAESKeyString));// used for debugging
             return encryptedAESKeyString;
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not encrypt Symmetric Key with the clients Public Key");
         }
         return null;
     }
 
     public void SendEncryptedAESKey() {
+        /* THis method assisgns to a vriable the value returned by the method AESKeyEncryptor()
+         * then it uses the buffered writer to send it though the socket
+         * Try chatch are implemented in case of errors */
         try {
         
             String encryptedAESKeyString = AESKeyEncryptor();
@@ -185,11 +208,16 @@ class ClientHandler implements Runnable {
             writer.newLine();
             writer.flush();
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not send encrypted Symmetric Key ");
         }
     }
 
     public String EncryptMessage(String message) {
+        /* This method uses the cipher class together with the Symmetric key gathered earlier
+         * to encrypt the message passed as a parameter it it also uses the base64 class to 
+         * convert the byte array to string and return it. It is all inside of a try catch and 
+         * for security reasons none of the errors print stacktrace of the error itself, instead
+         * they print a message*/
         String encryptedMessage = null;
         try {
             Cipher cipher = Cipher.getInstance("AES");
@@ -198,21 +226,30 @@ class ClientHandler implements Runnable {
             encryptedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
 
         } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException ex) {
-            ex.printStackTrace();
+            System.out.println("Error: Could not encrypt message with Symmetric Key ");
         }
         return encryptedMessage;
     }
 
     public String DecryptMessage(String message) {
+         /* This method uses the cipher class together with the Symmetric key stored globaly
+         * to decrypt the message passed as a parameter it uses the base64 class to 
+         * convert the String message to byte array and then after its passed through the cipher 
+         * and then byte array variable is converted to string which is then returned as the final decrypted message.
+         * It is all inside of a try catch and for security reasons none of the errors 
+         * print stacktrace of the error itself, instead they print a message*/
         String decryptedMessage = null;
         try {
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.DECRYPT_MODE, aesKey);
-            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(message));
+            byte[] decodedBytes = Base64.getDecoder().decode(message);
+        
+            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
             decryptedMessage = new String(decryptedBytes);
 
-        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
-            ex.printStackTrace();
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException
+                | NoSuchAlgorithmException | NoSuchPaddingException ex) {
+            System.out.println("Error: Could not decrypt message with Symmetric Key ");
         }
         return decryptedMessage;
     }
